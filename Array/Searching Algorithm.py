388b8524ec7ab1e75ecfd9d4678884ca96db8292
jammy25 <kishan95.jammy@gmail.com>
2022-06-10 07:00:26 +0530

    Binary search

--- Array/Searching Algorithm.py
+++ Array/Searching Algorithm.py
@@ -1,10 +1,16 @@
 # Find out if a key exists in the sorted list
 # array[left..right] or not using binary search algorithm
 def binarySearch(array, left, right, key):
- 
-    #Practise Yourself : write your code here
-    return None
-    
+    if left > right:
+        return -1
+    mid = left + ((right - left)//2)
+    if array[mid] == key:
+        return mid
+    elif key < array[mid]:
+        return binarySearch(array, left, mid - 1, key)
+    else:
+        return binarySearch(array, mid + 1, right, key)
+
 if __name__ == '__main__':
  
     array = [2, 5, 6, 8, 9, 10]
