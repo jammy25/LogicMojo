1715513487689aca2f66579de4d3b6590c76afb1
jammy25 <kishan95.jammy@gmail.com>
2022-06-30 10:56:52 +0530

    XOR method to find the element that occurs one

---
+++ Array/XOR method to find the element that occurs one.py
@@ -0,0 +1,16 @@
+def findSingle( arr, n): 
+    
+    result = arr[0]
+
+    for i in range(1, n):
+    	
+    	result = arr[i] ^ result 
+
+    return result 
+
+# Driver Code
+array = [[6,2,4,3,4,2,3],
+		  [4,1,2,1,2],
+		  [2, 2, 1]]
+for arr in array:
+	print ('Element is', findSingle(arr, len(arr)))
