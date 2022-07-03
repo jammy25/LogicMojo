715b77b0e501cd31d1d25da818a54ae03180fa50
jammy25 <kishan95.jammy@gmail.com>
2022-07-03 08:22:48 +0530

    Maximum value of arr[i] - arr[j]

--- Array/Maximum value of arr[i] - arr[j].py
+++ Array/Maximum value of arr[i] - arr[j].py
@@ -3,7 +3,7 @@
 
 def maxIndexDiff(arr, n):
 
-	maxDiff = -1
+	maxDiff = 0
 	
 	for i in range(n):
 		j = n - 1
@@ -20,28 +20,34 @@ def maxIndexDiff(arr, n):
 # Auxiliary Space: O(n)
 
 def findDiff(arr, n):
-	maxDiff = 0
+	MaxDiff = 0
 	leftMin = [0] * n
 	rightMax = [0] * n
 
-	leftMin[0] = 0
+	leftMin[0] = arr[0]
 	for i in range(1, n):
 		leftMin[i] = min(arr[i], leftMin[i - 1])
 
-	rightMax[n - 1] = n - 1
-	for j in range(n - 2, 0, -1):
+	rightMax[n - 1] = arr[n - 1]
+	for j in range(n - 2, -1, -1):
 		rightMax[j] = max(arr[j], rightMax[j + 1])
 
-	i, j, maxDiff = 0, 0, -1
+	i, j, MaxDiff = 0, 0, -1
 	while (j < n and i < n):
-		if (leftMin <= rightMax):
-			maxDiff = max(maxDiff, j - i)
+		if (leftMin[i] <= rightMax[j]):
+			MaxDiff = max(MaxDiff, j - i)
 			j = j + 1
 		else:
 			i = i + 1
-	return  maxDiff
+	return  MaxDiff
 
 # Driver Code
-arr = [34, 8, 10, 3, 2, 80, 30, 33, 1]
-print(maxIndexDiff(arr, len(arr)))
-print(findDiff(arr, len(arr)))
+array = [[34, 8, 10, 3, 2, 80, 30, 33, 1],
+		 [9, 10, 2, 6, 7, 12, 8, 1],
+         [35, 9, 12, 3, 2, 70, 31, 33, 1],
+         [1, 2, 3, 4, 5, 6],
+         [9, 2, 3, 4, 5, 6, 7, 8, 18, 0],
+         [6, 5, 4, 3, 2, 1]]
+for arr in array:
+	print(maxIndexDiff(arr, len(arr)))
+	print(findDiff(arr, len(arr)))
